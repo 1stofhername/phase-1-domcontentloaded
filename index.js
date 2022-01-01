7887b54bdf17617ef2e630ca0757913b6c95934b
Alicia <arrowsrp@yahoo.com>
2021-12-31 19:08:53 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let text = document.getElementById('text');
+    text.textContent = 'This is really cool!';
+    console.log('The DOM has loaded and parsed')
+})
+console.log('This parsed beforehand.')
+
 // Your code goes here
